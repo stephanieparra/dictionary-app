59e8eda744b180bdc270fde007dddd4d84a07bcc
stephanieparra <stephanieparrastudio@gmail.com>
2023-01-16 18:27:02 -0800

    Created dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from "./dictionary-app-img.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-img img-fluid" alt="dictionary-logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-img img-fluid" alt="dictionary-logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded and designed by Stephanie Parra.
+        </footer>
+      </div>
     </div>
   );
 }
