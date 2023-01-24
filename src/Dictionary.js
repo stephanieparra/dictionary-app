ea5e049577eb1f0b9fed81adf1815a1df5129b62
stephanieparra <stephanieparrastudio@gmail.com>
2023-01-23 18:08:28 -0800

    Styling for dictionary component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -29,6 +29,7 @@ export default function Dictionary() {
         <form onSubmit={search}>
           <input type="search" onChange={handleKeywordChange} />
         </form>
+        <div className="hint">suggested words: sunset, wine, yoga...</div>
       </section>
       <Results results={results} />
     </div>
