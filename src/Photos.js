7c2a9a62040c5d0161ac31e3ceb8704388b820bb
stephanieparra <stephanieparrastudio@gmail.com>
2023-01-24 13:05:19 -0800

    Edited img alt

--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="image of searched word"
+                    alt="searched word"
                   />
                 </a>
               </div>
