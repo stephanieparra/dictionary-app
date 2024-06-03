4367c66a42af81c22e3da29a40edccd17473d4b3
stephanieparra <stephanieparrastudio@gmail.com>
2024-06-02 20:07:39 -0700

    Updated note in dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -19,7 +19,7 @@ export default function Dictionary(props) {
   }
 
   function search() {
-    // documentation: https://dictionaryapi.dev/
+    // documentation: dictionaryapi.dev & SheCodes Image API //
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
 
